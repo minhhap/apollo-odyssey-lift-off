79f05e03b1917fa3901758d6d7940aced3897a49
Pham <minhha01vn@yahoo.com>
2021-12-30 10:34:58 -0600

    fixed track-api

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,14 +1,16 @@
 const resolvers = {
     Query: {
-        // returns an array of Tracks that will be used to populate
-        // the homepage grid of our web client
-        tracksForHome: (_, __, {dataSources}) => {
-            return dataSources.trackAPI.getTracksForHome();
-        }
+      // get all tracks, will be used to populate the homepage grid of our web client
+      tracksForHome: (_, __, {dataSources}) => {
+        return dataSources.trackAPI.getTracksForHome();
+      }
     },
     Track: {
-        author: (parent, _, {dataSources}) => {}
+      author: ({authorId}, _, {dataSources}) => {
+        return dataSources.trackAPI.getAuthor(authorId);
       }
-};
+    }
+  };
 
 module.exports = resolvers;
+
