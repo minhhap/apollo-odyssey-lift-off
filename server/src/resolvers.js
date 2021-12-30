2f77c51d2d85c0fe0fb0d9dc5d710ed040681d53
Pham <minhha01vn@yahoo.com>
2021-12-30 09:34:19 -0600

    added resolver.js

---
+++ server/src/resolvers.js
@@ -0,0 +1,14 @@
+const resolvers = {
+    Query: {
+        // returns an array of Tracks that will be used to populate
+        // the homepage grid of our web client
+        tracksForHome: (_, __, {dataSources}) => {
+            return dataSources.trackAPI.getTracksForHome();
+        }
+    },
+    Track: {
+        author: (parent, _, {dataSources}) => {}
+      }
+};
+
+module.exports = resolvers;
